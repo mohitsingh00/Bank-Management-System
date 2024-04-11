1b20767034a8e75172e34890983cbdda0b2f3f48
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-11 12:24:10 +0530

    Registration added

--- src/com/bank/User.java
+++ src/com/bank/User.java
@@ -1,6 +1,8 @@
 package com.bank;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class User {
@@ -12,6 +14,45 @@ public class User {
 	{
 		this.connection = conneciton;
 		this.scanner = scanner;
+	}
+	
+	public void register()
+	{
+		scanner.nextLine();
+		System.out.println("Full Name: ");
+		String full_name = scanner.nextLine();
+		System.out.println("Email: ");
+		String email = scanner.nextLine();
+		System.out.println("Password: ");
+		String password = scanner.nextLine();
+		
+		
+		String register_query = "INSERT INTO User(full_name, email, password) VALUES(?,?,?)";
+		try
+		{
+			PreparedStatement preparedStatement = connection.prepareStatement(register_query);
+			preparedStatement.setString(1, full_name);
+			preparedStatement.setString(2, email);
+			preparedStatement.setString(3, password);
+			int affectedRows = preparedStatement.executeUpdate();
+			if(affectedRows > 0)
+			{
+				System.out.println("Registration Successfull!");
+			}
+			else
+			{
+				System.out.println("Registration Failed!");
+			}
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+	public void user_exist()
+	{
+		
 	}
 
 }
