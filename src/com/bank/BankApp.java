a4b9a10b888537a2b8bc33395a2dd44bdf4156be
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-11 11:50:52 +0530

    Database Connection added

--- src/com/bank/BankApp.java
+++ src/com/bank/BankApp.java
@@ -1,5 +1,33 @@
 package com.bank;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class BankApp {
+	
+	private static final String url = "jdbc:mysql://localhost:3306/carrentalsystem";
+	private static final String username = "root";
+	private static final String password = "Mohit@123";
+	
+	public static void main(String[] args) {
+		try
+		{
+			Class.forName("com.mysql.cj.jdbc.Driver");
+		}
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		try
+		{
+			Connection connection = DriverManager.getConnection(url, username, password);
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+	}
 
 }
