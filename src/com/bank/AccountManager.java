c6fa3ed080879310ce89405d11ab0a80e72d0fbb
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-11 18:09:59 +0530

    Debit Money Function added

--- src/com/bank/AccountManager.java
+++ src/com/bank/AccountManager.java
@@ -1,6 +1,9 @@
 package com.bank;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class AccountManager {
@@ -13,4 +16,63 @@ public class AccountManager {
 		this.connection = conneciton;
 		this.scanner = scanner;
 	}
+	
+	public void debit_money(long account_number) throws SQLException
+	{
+		System.out.println("Enter Amount: ");
+		double amount = scanner.nextDouble();
+		scanner.nextLine();
+		System.out.println("Enter Security Pin: ");
+		String security_pin = scanner.nextLine();
+		try
+		{
+			if(account_number != 0)
+			{
+				connection.setAutoCommit(false);
+				PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Accounts WHERE account_number = ? AND security_pin = ?");
+				preparedStatement.setLong(1, account_number);
+				preparedStatement.setString(2, security_pin);
+				ResultSet resultSet = preparedStatement.executeQuery();
+				if(resultSet.next())
+				{
+					double current_balance = resultSet.getDouble("balance");
+					if(amount <= current_balance)
+					{
+						String credit_query = "UPDATE Accounts SET balance = balance - ? WHERE account_number = ?";
+						PreparedStatement preparedStatement1 = connection.prepareStatement(credit_query);
+						preparedStatement1.setDouble(1, amount);
+						preparedStatement1.setLong(2, account_number);
+						int rowsAffected = preparedStatement.executeUpdate();
+						if(rowsAffected > 0)
+						{
+							System.out.println("RS."+amount+" debited Successfully");
+							connection.commit();
+							connection.setAutoCommit(true);
+							return;
+						}
+						else
+						{
+							System.out.println("Transection Failed!");
+							connection.rollback();
+							connection.setAutoCommit(false);
+						}
+					}
+					else
+					{
+						System.out.println("Insufficient Balance!");
+					}
+				}
+				else
+				{
+					System.out.println("Invalid Pin!");
+				}
+			}
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		connection.setAutoCommit(true);
+	}
+	
 }
