f45297cc2d6fbaa631199593d05c7d81963970c7
mohitsingh00 <mohitsingh96210@gmail.com>
2024-04-11 16:01:29 +0530

    Account Exist Function added

--- src/com/bank/Accounts.java
+++ src/com/bank/Accounts.java
@@ -1,6 +1,9 @@
 package com.bank;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Accounts {
@@ -13,4 +16,29 @@ public class Accounts {
 		this.connection = conneciton;
 		this.scanner = scanner;
 	}
+	
+	public boolean account_exist(String email)
+	{
+		String query = "SELECT account_number FROM Accounts WHERE email = ?";
+		try
+		{
+			PreparedStatement preparedStatement = connection.prepareStatement(query);
+			preparedStatement.setString(1, email);
+			ResultSet resultSet = preparedStatement.executeQuery();
+			if(resultSet.next())
+			{
+				return true;
+			}
+			else
+			{
+				return false;
+			}
+		}
+		catch (SQLException e)
+		{
+			e.printStackTrace();
+		}
+		return false;
+	}
+	
 }
